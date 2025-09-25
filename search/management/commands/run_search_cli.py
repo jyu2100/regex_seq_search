fcc4ade27e39368ea284e806cf8761493496408e
Jason Yu <jyu2100@gmail.com>
2025-09-24 22:07:05 -0400

    Add a command line tool as a django management command

---
+++ search/management/commands/run_search_cli.py
@@ -0,0 +1,31 @@
+from django.core.management.base import BaseCommand
+from search.utils import run_search
+import json
+
+class Command(BaseCommand):
+    help = "Nucleotide sequence search from the command line"
+
+    def add_arguments(self, parser):
+        parser.add_argument("pattern", type=str, help="Regex pattern")
+        parser.add_argument("uid", type=str, help="UID")
+
+    def handle(self, *args, **options):
+        try:
+            pattern = options["pattern"]
+            uid = options["uid"]
+
+            data = run_search(pattern, uid)
+            matches = json.loads(data)
+
+            for sequence in matches:
+                self.stdout.write(self.style.SUCCESS(f'{sequence}'))
+
+                for start, end in matches[sequence]:
+                    self.stdout.write(self.style.SUCCESS(f'  {start}-{end}'))
+
+                self.stdout.write(self.style.SUCCESS(''))
+
+        except Exception as e:
+            self.stdout.write(self.style.ERROR(e))
+
+
