7adfd7d8c560644d1f51e9a273c9f3d936ac4d6b
Jason Yu <jyu2100@gmail.com>
2025-09-25 10:33:02 -0400

    Move SECRET_KEY to .env

--- regex_seq_search/settings.py
+++ regex_seq_search/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
+from decouple import config
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,10 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-bug_v=8eihc6c_917tu9y*qx!ict_etrg)!r1c)$za_!*8(oyr'
+# SECRET_KEY = 'django-insecure-bug_v=8eihc6c_917tu9y*qx!ict_etrg)!r1c)$za_!*8(oyr'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
+DEBUG = config('DEBUG')
 
 ALLOWED_HOSTS = []
 
